967498f04b376161a9eb0605efa54bea66987005
Victor Alonso <victor@ieselcaminas.org>
2019-04-11 15:40:36 +0200

    Added init game, clear board and fill randomly

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -27,7 +27,13 @@ public class MainFrame extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        board1 = new Board();
+        board = new Board();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItemInitGame = new javax.swing.JMenuItem();
+        jMenuItemClearBoard = new javax.swing.JMenuItem();
+        jMenuItemFillRandom = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(153, 255, 204));
@@ -42,26 +48,71 @@ public class MainFrame extends javax.swing.JFrame {
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 331, Short.MAX_VALUE)
+            .addGap(0, 279, Short.MAX_VALUE)
         );
 
         getContentPane().add(jPanel1, java.awt.BorderLayout.LINE_END);
 
-        javax.swing.GroupLayout board1Layout = new javax.swing.GroupLayout(board1);
-        board1.setLayout(board1Layout);
-        board1Layout.setHorizontalGroup(
-            board1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout boardLayout = new javax.swing.GroupLayout(board);
+        board.setLayout(boardLayout);
+        boardLayout.setHorizontalGroup(
+            boardLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 307, Short.MAX_VALUE)
         );
-        board1Layout.setVerticalGroup(
-            board1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 331, Short.MAX_VALUE)
+        boardLayout.setVerticalGroup(
+            boardLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 279, Short.MAX_VALUE)
         );
 
-        getContentPane().add(board1, java.awt.BorderLayout.CENTER);
+        getContentPane().add(board, java.awt.BorderLayout.CENTER);
+
+        jMenu1.setText("File");
+
+        jMenuItemInitGame.setText("Init Game");
+        jMenuItemInitGame.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItemInitGameActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItemInitGame);
+
+        jMenuItemClearBoard.setText("Clear board");
+        jMenuItemClearBoard.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItemClearBoardActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItemClearBoard);
+
+        jMenuItemFillRandom.setText("Fill board randomly");
+        jMenuItemFillRandom.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItemFillRandomActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItemFillRandom);
+
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Edit");
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuItemInitGameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItemInitGameActionPerformed
+        board.initGame();
+    }//GEN-LAST:event_jMenuItemInitGameActionPerformed
+
+    private void jMenuItemClearBoardActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItemClearBoardActionPerformed
+        board.clearBoard();
+    }//GEN-LAST:event_jMenuItemClearBoardActionPerformed
+
+    private void jMenuItemFillRandomActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItemFillRandomActionPerformed
+        board.fillRandomly(0.2f);
+    }//GEN-LAST:event_jMenuItemFillRandomActionPerformed
 
     /**
      * @param args the command line arguments
@@ -99,7 +150,13 @@ public class MainFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private Board board1;
+    private Board board;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItemClearBoard;
+    private javax.swing.JMenuItem jMenuItemFillRandom;
+    private javax.swing.JMenuItem jMenuItemInitGame;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
